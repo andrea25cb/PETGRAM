9403cffa730dd2949bc20707918df55065ab1a61
Andrea Cordón Barrionuevo <92718546+andrea25cb@users.noreply.github.com>
2023-05-17 09:46:42 +0200

    Update profile.blade.php

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -17,9 +17,9 @@
                     </div>
                     <p class="fs-4">{{ $user->bio }}</p>
                     @if(Auth::id() !== $user->id)
-                        <form action="{{ route('followUser', $user->id) }}" method="POST">
+                        <form id="followForm" action="{{ route('followUser', $user->id) }}" method="POST">
                             @csrf
-                            <button class="btn btn-primary">{{ $isFollowing ? 'Unfollow' : 'Follow' }}</button>
+                            <button id="followButton" class="btn btn-primary">{{ $isFollowing ? 'Unfollow' : 'Follow' }}</button>
                         </form>
                     @endif
                 </div>
@@ -46,5 +46,54 @@
         </div>
     </div>
 </div>
+
+<!-- Unfollow Modal -->
+<div class="modal fade" id="unfollowModal" tabindex="-1" aria-labelledby="unfollowModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="unfollowModalLabel">Unfollow User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to unfollow this user?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <form id="unfollowForm" action="{{ route('unfollowUser', $user->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Unfollow</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <br><br><br>
 @endsection
+
+@push('scripts')
+<script>
+    // Handle Unfollow button click event
+    const unfollowButton = document.getElementById('followButton');
+    const unfollowForm = document.getElementById('unfollowForm');
+    const unfollowModal = new bootstrap.Modal(document.getElementById('unfollowModal'));
+
+    unfollowButton.addEventListener('click', function() {
+        unfollowModal.show();
+    });
+
+    // Handle Unfollow modal cancel button click event
+    const unfollowModalCancel = document.querySelector('#unfollowModal button[data-bs-dismiss="modal"]');
+    unfollowModalCancel.addEventListener('click', function() {
+        unfollowModal.hide();
+    });
+
+    // Handle Unfollow form submit event
+    unfollowForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        unfollowModal.hide();
+        this.submit();
+    });
+</script>
+@endpush
